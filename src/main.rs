9e58e04303936530376e23d5c6838bba04c8b5a6
otretas <ofilipejorge@gmail.com>
2022-01-08 22:42:12 +0000

    updated

--- src/main.rs
+++ src/main.rs
@@ -31,18 +31,19 @@ fn main() {
                                     format!("cd \"{}\"\ncargo build", args[1]));
         }
 
+        let argument = format!("{} \"cd \"{}\" & cargo build\"", cmd_arg, args[1]);
+        println!("{}", argument);
         // Executing cargo build on directory
         let output =  Command::new(&cmdline)
-                            .arg(&cmd_arg)
-                            .arg(&filename)
-                            //.arg(format!("cd \"{}\"", args[1]))
-                            //.arg("cargo build")
+                            .arg(argument)
+                            //.arg(&cmd_arg)
+                            //.arg(&filename)
+                            .arg("cargo build")
                             .output()
                             .expect("failed to execute process");
 
         println!("Executing in cmd:");
         println!("{}", output.status);
-        //println!("{}", String::from_utf8_lossy(&output.stdout));
         println!("{}", String::from_utf8_lossy(&output.stderr));
 
         // Read configuration
