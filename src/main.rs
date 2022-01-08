2ceb844b1f34e85a6db55a6b61a1fa59f5c7881d
otretas <ofilipejorge@gmail.com>
2022-01-08 19:14:13 +0000

    adding arguments

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,26 @@
 use std::env;
+use std::process::Command;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    executable = args[1];
-    println!("The arguments are:");
-    /*for arg in args{
+    // Executing batch/sh selected file
+    println!("Executing in cmd:");
+    for arg in &args[1..]{
         println!("{}", arg);
-    }*/
+    }
+    let output = if cfg!(target_os = "windows"){
+        Command::new("cmd")
+                .args(&args[1..])
+                .output()
+                .expect("failed to execute process")
+    }
+    else{
+        Command::new("sh")
+                .args(&args[1..])
+                .output()
+                .expect("failed to execute process")
+    };
 
+    println!("{}", output.status);
 }
