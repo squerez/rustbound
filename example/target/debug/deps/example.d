603cdfdd22e11773f597de66ce981489e3b6bf7d
otretas <ofilipejorge@gmail.com>
2022-01-08 21:32:34 +0000

    removed targets

--- example/target/debug/deps/example.d
+++
@@ -1,5 +0,0 @@
-C:\Users\filip\OneDrive - IPLeiria\CDRSP\Rust\rustbound\example\target\debug\deps\example.exe: src\main.rs
-
-C:\Users\filip\OneDrive - IPLeiria\CDRSP\Rust\rustbound\example\target\debug\deps\example.d: src\main.rs
-
-src\main.rs:
