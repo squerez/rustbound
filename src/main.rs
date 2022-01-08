735c2b84e7aba48c1b114d20b3cbaa3bacd8e4ee
svacx <jpsantos93@hotmail.com>
2022-01-08 19:59:56 +0000

    added read configuration file

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,11 @@
+use configparser::ini::Ini;
+
 fn main() {
     let number = 123;
     println!("Hello, world! {}", number);
+    
+    // Read configuration
+    let mut config = Ini::new();
+    let map = config.load("../conf/app_settings.ini");
+    println!("{:?}", map);
 }
