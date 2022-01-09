a28faca0c4436bc97e275d7fb063cadce2f13d4e
svacx <jpsantos93@hotmail.com>
2022-01-09 00:12:37 +0000

    unified approach

--- src/main.rs
+++ src/main.rs
@@ -7,22 +7,24 @@ fn main() {
 
     if args.len()< 2 {
         println!("It needs the directory of the project passed as an argument");
-    }
-    else{
+    } else {
+        let cmdstr = format!("cd {} && cargo build", args[1]);
+        
         let cmdline;
         let argument;
+        
+        // Different configs for windows or linux
         if cfg!(target_os = "windows"){
             cmdline = "cmd";
-            argument = format!("/K cd {} & cargo build", args[1]);
-        }
-        else{
-            cmdline = "/bin/bash";
-            argument = format!("cd {} && cargo build", args[1]);
-        }
+            argument = ["/K", &cmdstr] 
+        } else {
+            cmdline = "/usr/bin/sh";
+            argument = ["-c", &cmdstr] 
+       }
 
         // Executing cargo build on directory
-        let output =  Command::new(&cmdline)
-                                .arg(argument)
+        let output =  Command::new(cmdline)
+                                .args(&argument)
                                 .output()
                                 .expect("failed to execute process");
 
